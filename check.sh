eaba4a7042360c744fa6327b7407e7a650bff70e
xxmawhu <xxmawhu@163.com>
2019-09-27 20:30:07 +0800

    add check.sh

---
+++ check.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# ====================================================
+#   Copyright (C)2019 All rights reserved.
+#
+#   Author        : Xin-Xin MA
+#   Email         : xxmawhu@163.com
+#   File Name     : check.sh
+#   Created Time  : 2019-09-27 20:29
+#   Last Modified : 2019-09-27 20:29
+#   Describe      :
+#
+# ====================================================
+
+
+
